182e5d6b06a09511255beb0625ffcb41fc776a47
Martin Okem Odachi <odaci054@gmail.com>
2023-09-15 07:04:15 +0000

    standard I/O

---
+++ stdio.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{	
+	int age;
+	printf("this is used to print\n");
+	printf("The Scanf function does not display output on screen rather it recieves input\n");
+	printf("Enter your age: ");
+	scanf(" %d ",&age);
+       return (0);	
+}
